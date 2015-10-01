ce11ea80a71316a9a93126dded2e4456342f0557
Craeckie <josia-internet@rosskopfs.de>
2015-10-01 19:28:35 +0200

    trying to use link-configuration for set up

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
 set -e
-
-DB_TYPE=${DB_TYPE:-sqlite}
-DB_HOST=${DB_HOST:-localhost}
-DB_NAME=${DB_NAME:-owncloud}
-DB_USER=${DB_USER:-owncloud}
-DB_PASS=${DB_PASS:-owncloud}
+if [ -n "$DB_HOST" ]; then
+    echo "Using parametric configuration" 1>&2
+    DB_TYPE=${DB_TYPE:-sqlite}
+    DB_HOST=${DB_HOST:-localhost}
+    DB_NAME=${DB_NAME:-owncloud}
+    DB_USER=${DB_USER:-owncloud}
+    DB_PASS=${DB_PASS:-owncloud}
+elif [ -n "$MYSQL_PORT_3306_TCP_ADDR" ]; then
+    echo "Using link configuration" 1>&2
+    DB_TYPE=mysql
+    DB_HOST=$MYSQL_PORT_3306_TCP_ADDR
+    DB_NAME=${MYSQL_ENV_MYSQL_DATABASE:-owncloud}
+    DB_USER=${MYSQL_ENV_MYSQL_USER:-owncloud}
+    DB_PASS=${MYSQL_ENV_MYSQL_PASSWORD:-owncloud}
+fi
 DB_TABLE_PREFIX=${DB_TABLE_PREFIX:-oc_}
 ADMIN_USER=${ADMIN_USER:-admin}
 ADMIN_PASS=${ADMIN_PASS:-changeme}
