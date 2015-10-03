89fb3908ee5614325404312ebce4212af8447736
Craeckie <josia-internet@rosskopfs.de>
2015-10-03 16:24:11 +0200

    fixing config-volume

--- run.sh
+++ run.sh
@@ -158,14 +158,18 @@ update_nginx_config
 # Create data directory
 mkdir -p "$DATA_DIR"
 
+# Fix volumes
+
 # Fix apps-volume
 if find "$APPS_DIR" -maxdepth 0 -empty | read v; then
     echo -n "Fixing apps-volume in $APPS_DIR... "
     tar -xzf /tmp/owncloud.tar.gz -C "$APPS_DIR" --strip-components=2 core-${OWNCLOUD_VERSION}/apps
     [[ $? -eq 0 ]] && echo "Done !" || echo "FAILURE"
-    
+fi
+# Fix config-volume
+if find "$CONFIG_DIR" -maxdepth 0 -empty | read v; then
     echo -n "Fixing missing ca-bundle.crt in $CONFIG_DIR... "
-    tar -xzf /tmp/owncloud.tar.gz -C $CONFIG_DIR --strip-components=2 core-${OWNCLOUD_VERSION}/config/ca-bundle.crt
+    tar -xzf /tmp/owncloud.tar.gz -C $CONFIG_DIR --strip-components=2 core-${OWNCLOUD_VERSION}/config
     [[ $? -eq 0 ]] && echo "Done !" || echo "FAILURE"
 fi
 
